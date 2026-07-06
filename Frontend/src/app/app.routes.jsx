bc345517062003a8e100ab9312257d117d2c006f
satwaj <satwajbachhav2004@gmail.com>
2026-07-06 10:04:39 +0530

    refactor(routes): standardize import statements and formatting in app.routes.jsx

--- Frontend/src/app/app.routes.jsx
+++ Frontend/src/app/app.routes.jsx
@@ -1,77 +1,75 @@
-import { createBrowserRouter } from "react-router"
-import Register from "../features/auth/Pages/Register"
-import Login from "../features/auth/Pages/Login"
-import CreateProduct from "../features/products/pages/createProduct"
-import Dashboard from "../features/products/pages/Dashboard"
-import Protected from "../features/auth/components/Protected"
-import Home from "../features/products/pages/Home"
-import Productdetail from "../features/products/pages/Productdetail"
-import SellerProductDetails from "../features/products/pages/SellerProductDetails"
-import Cart from "../features/cart/pages/Cart"
-import OrderSuccess from "../features/cart/pages/OrderSuccess"
-import AppLayout from "./AppLayout"
+import { createBrowserRouter } from "react-router";
+import Register from "../features/auth/Pages/Register";
+import Login from "../features/auth/Pages/Login";
+import CreateProduct from "../features/products/pages/createProduct";
+import Dashboard from "../features/products/pages/Dashboard";
+import Protected from "../features/auth/components/Protected";
+import Home from "../features/products/pages/Home";
+import Productdetail from "../features/products/pages/Productdetail";
+import SellerProductDetails from "../features/products/pages/SellerProductDetails";
+import Cart from "../features/cart/pages/Cart";
+import OrderSuccess from "../features/cart/pages/OrderSuccess";
+import AppLayout from "./AppLayout";
 
+export const routes = createBrowserRouter([
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/product/:productId",
+        element: <Productdetail />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order-success",
+        element: <OrderSuccess />,
+      },
 
+      {
+        path: "/seller",
+        children: [
+          {
+            path: "/seller/create-product",
+            element: (
+              <Protected role="seller">
+                <CreateProduct />
+              </Protected>
+            ),
+          },
 
- export const routes = createBrowserRouter([
-   {
-     path: "/register",
-     element: <Register />,
-   },
-   {
-     path: "/login",
-     element: <Login />,
-   },
-   {
-    element: <AppLayout/>,
-     children: [
-       {
-         path: "/",
-         element: <Home />,
-       },
-       {
-         path: "/product/:productId",
-         element: <Productdetail />,
-       },
-       {
-         path: "/cart",
-         element: <Cart />,
-       },
-       {
-         path: "/order-success",
-         element: <OrderSuccess />,
-       },
-
-       {
-         path: "/seller",
-         children: [
-           {
-             path: "/seller/create-product",
-             element: (
-               <Protected role="seller">
-                 <CreateProduct />
-               </Protected>
-             ),
-           },
-
-           {
-             path: "/seller/dashboard",
-             element: (
-               <Protected role="seller">
-                 <Dashboard />
-               </Protected>
-             ),
-           },
-           {
-             path: "/seller/product/:productId",
-             element: (
-               <Protected role="seller">
-                 <SellerProductDetails />
-               </Protected>
-             ),
-           },
-         ],
-       },
-     ],
-   },
- ]);
+          {
+            path: "/seller/dashboard",
+            element: (
+              <Protected role="seller">
+                <Dashboard />
+              </Protected>
+            ),
+          },
+          {
+            path: "/seller/product/:productId",
+            element: (
+              <Protected role="seller">
+                <SellerProductDetails />
+              </Protected>
+            ),
+          },
+        ],
+      },
+    ],
+  },
+]);
